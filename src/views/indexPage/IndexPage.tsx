8b9d7f1a115f4d93da8a6166ab7fe75e67b55964
KevinKVJ <jiakaiwen5216@gmail.com>
2023-02-04 01:03:59 -0700

    Change some styles in index page

--- src/views/indexPage/IndexPage.tsx
+++ src/views/indexPage/IndexPage.tsx
@@ -1,11 +1,14 @@
 import { css } from '@emotion/react';
 
-import Navbar from '../common/Navbar';
 import Article from '../common/Article';
 import ArticleSection from '../common/ArticleSection';
+import Navbar from '../common/Navbar';
 
 const IndexPage = () => {
     const indexPageStyles = css`
+        background-color: #eeeeee;
+        min-height: 100vh;
+
         > .content {
             max-width: 1240px;
             /* outline: 1px solid; */
